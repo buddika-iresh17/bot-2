e54e158aff2f211d75ea2fcf272e0eb227e17330
buddika-iresh <buddikairesh427@gmail.com>
2025-07-12 16:57:15 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,22 @@
+function convertToBool(text, fault = 'true') {
+  return text === fault;
+}
+
+module.exports = {
+    SESSION_ID: process.env.SESSION_ID || "BycRgDja#LWoUdcYkpYXQCbqPUJEondSin_M2-BUs-faYy6Hv8Iw",
+    MODE: process.env.MODE || "private",
+    PREFIX: process.env.PREFIX || ".",
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
+    DEV: process.env.DEV || "94721551183",
+    READ_MESSAGE: process.env.READ_MESSAGE || "false",
+    AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "false",
+    AUTOLIKESTATUS: process.env.AUTOLIKESTATUS || "false",
+    AUTO_TYPING: process.env.AUTO_TYPING || "true",
+    AUTO_RECORDING: process.env.AUTO_RECORDING || "true",
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
+    UNIFIED_PROTECTION: process.env.UNIFIED_PROTECTION || "kick", // "off" | "warn" | "kick" | "strict"
+    };
+    
